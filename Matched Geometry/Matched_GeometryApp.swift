2ff3a146736238900d22802ac5d4bccb775a198f
John James <jjames@pacbell.net>
2020-07-30 12:08:42 -0700

    Initial Commit

---
+++ Matched Geometry/Matched_GeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Matched_GeometryApp.swift
+//  Matched Geometry
+//
+//  Created by John James Retina on 7/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Matched_GeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
